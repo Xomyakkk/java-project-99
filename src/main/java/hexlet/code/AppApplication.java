8022c29f173f8d0febdff5bdda8eab1ff3156038
Xomyakkk <megaxomyak@gmail.com>
2026-06-20 18:38:23 +0300

    Fix AppApplication

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,7 +1,11 @@
 package hexlet.code;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "hexlet.code")
 public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }
