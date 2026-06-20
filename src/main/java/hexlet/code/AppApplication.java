4f98d28218e351a010e815b5bd86524c053b7c7a
Xomyakkk <megaxomyak@gmail.com>
2026-06-20 10:57:31 +0300

    Add AppApplication

---
+++ src/main/java/hexlet/code/AppApplication.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "hexlet.code")
+public class AppApplication {
+}
